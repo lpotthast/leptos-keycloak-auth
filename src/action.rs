c72b2b06ad4edc3fb1121b4a40f7b1a798c2509a
Lukas Potthast <privat@lukas-potthast.de>
2025-01-12 14:54:18 +0100

    Wrap non-send actions in `spawn_local`

--- src/action.rs
+++ src/action.rs
@@ -18,19 +18,21 @@ pub(crate) fn create_retrieve_oidc_config_action(
     Action::new(move |(): &()| {
         let discovery_endpoint_url = discovery_endpoint_url.clone();
         async move {
-            let result = request::retrieve_oidc_config(discovery_endpoint_url).await;
-            match result {
-                Ok(oidc_config) => {
-                    set_oidc_config_wt.run(Some(OidcConfigWithTimestamp {
-                        oidc_config,
-                        retrieved: OffsetDateTime::now_utc(),
-                    }));
+            leptos::task::spawn_local(async move {
+                let result = request::retrieve_oidc_config(discovery_endpoint_url).await;
+                match result {
+                    Ok(oidc_config) => {
+                        set_oidc_config_wt.run(Some(OidcConfigWithTimestamp {
+                            oidc_config,
+                            retrieved: OffsetDateTime::now_utc(),
+                        }));
+                    }
+                    Err(err) => {
+                        tracing::error!(?err, "Could not retrieve OIDC config through discovery.");
+                        set_req_error.run(Some(err));
+                    }
                 }
-                Err(err) => {
-                    tracing::error!(?err, "Could not retrieve OIDC config through discovery.");
-                    set_req_error.run(Some(err));
-                }
-            }
+            });
         }
     })
 }
@@ -42,19 +44,21 @@ pub(crate) fn create_retrieve_jwk_set_action(
     Action::new(move |jwk_set_endpoint: &JwkSetEndpoint| {
         let jwk_set_endpoint = jwk_set_endpoint.clone();
         async move {
-            let result = request::retrieve_jwk_set(jwk_set_endpoint).await;
-            match result {
-                Ok(jwk_set) => {
-                    set_jwk_set_wt.run(Some(JwkSetWithTimestamp {
-                        jwk_set,
-                        retrieved: OffsetDateTime::now_utc(),
-                    }));
+            leptos::task::spawn_local(async move {
+                let result = request::retrieve_jwk_set(jwk_set_endpoint).await;
+                match result {
+                    Ok(jwk_set) => {
+                        set_jwk_set_wt.run(Some(JwkSetWithTimestamp {
+                            jwk_set,
+                            retrieved: OffsetDateTime::now_utc(),
+                        }));
+                    }
+                    Err(err) => {
+                        tracing::error!(?err, "Could not retrieve JWK set.");
+                        set_req_error.run(Some(err));
+                    }
                 }
-                Err(err) => {
-                    tracing::error!(?err, "Could not retrieve JWK set.");
-                    set_req_error.run(Some(err));
-                }
-            }
+            });
         }
     })
 }
@@ -76,22 +80,24 @@ pub(crate) fn create_exchange_code_for_token_action(
             let code = code.clone();
             let session_state = session_state.clone();
             async move {
-                let result = request::exchange_code_for_token(
-                    client_id,
-                    redirect_uri,
-                    token_endpoint,
-                    code,
-                    session_state,
-                )
-                .await;
-                match result {
-                    Ok(token) => {
-                        set_token.run(Some(token));
+                leptos::task::spawn_local(async move {
+                    let result = request::exchange_code_for_token(
+                        client_id,
+                        redirect_uri,
+                        token_endpoint,
+                        code,
+                        session_state,
+                    )
+                    .await;
+                    match result {
+                        Ok(token) => {
+                            set_token.run(Some(token));
+                        }
+                        Err(err) => {
+                            set_req_error.run(Some(err));
+                        }
                     }
-                    Err(err) => {
-                        set_req_error.run(Some(err));
-                    }
-                }
+                });
             }
         },
     )
@@ -108,10 +114,12 @@ pub(crate) fn create_refresh_token_action(
             let token_endpoint = token_endpoint.clone();
             let refresh_token = refresh_token.clone();
             async move {
-                match request::refresh_token(client_id, token_endpoint, refresh_token).await {
-                    Ok(refreshed_token) => set_token.run(Some(refreshed_token)),
-                    Err(err) => set_req_error.run(Some(err)),
-                }
+                leptos::task::spawn_local(async move {
+                    match request::refresh_token(client_id, token_endpoint, refresh_token).await {
+                        Ok(refreshed_token) => set_token.run(Some(refreshed_token)),
+                        Err(err) => set_req_error.run(Some(err)),
+                    }
+                });
             }
         },
     )
